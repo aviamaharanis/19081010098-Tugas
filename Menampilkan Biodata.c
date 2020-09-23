d4708989945b835ce526df1b7449fb6f21c84db8
aviamaharanis <aviamaharanis@gmail.com>
2020-09-23 11:50:52 +0700

    update upto 23.09.2020

---
+++ Menampilkan Biodata.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf("\tBIODATA PRIBADI\n\n");
+	printf("Nama	: Avia Maharani Situmorang\n");
+	printf("NPM	: 19081010098\n");
+	printf("Prodi	: Teknik Informatika\n");
+	printf("TTL	: Sidoarjo, 1 Juni 2001\n");
+	printf("Alamat	: Puri Indah, Sidoarjo");
+}
